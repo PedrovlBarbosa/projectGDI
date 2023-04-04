2248d8669a8cf45aee4c21d1292a2eedf2f66a54
Romero <rrcb@cin.ufpe.br>
2023-04-03 22:12:10 -0300

    insert phones

--- OR/povoamento.sql
+++ OR/povoamento.sql
@@ -3,3 +3,9 @@ INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('2222', '
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('3333', '33', 'Bairro 3', 'rua 3', 'em frente ao zoologico'); 
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('4444', '44', 'Bairro 4', 'rua 4', 'em frente ao presidio'); 
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('5555', '55', 'Bairro 5', 'rua 5', 'em frente ao predio'); 
+
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1111', '111111111', '81')
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('2222', '222222222', '81')
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('3333', '333333333', '81')
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('4444', '444444444', '81')
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('5555', '555555555', '81')
