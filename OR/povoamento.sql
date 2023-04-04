2a8598a5cf9dbcbd8e754a720ea773c2e195a97a
Douglas Santos <55150864+userdouglas@users.noreply.github.com>
2023-04-04 19:35:54 -0300

    Update povoamento.sql

--- OR/povoamento.sql
+++ OR/povoamento.sql
@@ -4,6 +4,7 @@ INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('3333', '
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('4444', '44', 'Bairro 4', 'rua 4', 'em frente ao presidio'); 
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('5555', '55', 'Bairro 5', 'rua 5', 'em frente ao predio'); 
 
+------------------------------------------ não precisa ----------------------------
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1111', '111111111', '81');
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('2222', '222222222', '81');
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('3333', '333333333', '81');
@@ -15,3 +16,7 @@ INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('2222', '3222
 INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('3333', '433333333', '82');
 INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('4444', '544444444', '16');
 INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('5555', '655555555', '81');
+--------------------------------------------------------------------------------------------
+
+
+
