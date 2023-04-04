1da12d65e56dc99894930d54f20763566ae6cc54
Romero <rrcb@cin.ufpe.br>
2023-04-03 22:16:35 -0300

    add ; to commands

--- OR/povoamento.sql
+++ OR/povoamento.sql
@@ -4,14 +4,14 @@ INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('3333', '
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('4444', '44', 'Bairro 4', 'rua 4', 'em frente ao presidio'); 
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('5555', '55', 'Bairro 5', 'rua 5', 'em frente ao predio'); 
 
-INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1111', '111111111', '81')
-INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('2222', '222222222', '81')
-INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('3333', '333333333', '81')
-INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('4444', '444444444', '81')
-INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('5555', '555555555', '81')
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1111', '111111111', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('2222', '222222222', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('3333', '333333333', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('4444', '444444444', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('5555', '555555555', '81');
 
-INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('1111', '211111111', '81')
-INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('2222', '322222222', '91')
-INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('3333', '433333333', '82')
-INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('4444', '544444444', '16')
-INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('5555', '655555555', '81')
+INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('1111', '211111111', '81');
+INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('2222', '322222222', '91');
+INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('3333', '433333333', '82');
+INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('4444', '544444444', '16');
+INSERT INTO tb_telefone_pessoa(cnpj_fabrica, numero, ddd) VALUES ('5555', '655555555', '81');
