59129179daa2820b50986841baef3dabd093da0d
Romero <rrcb@cin.ufpe.br>
2023-04-03 22:00:07 -0300

    start povoamento

---
+++ OR/povoamento.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('1111', '11', 'Bairro 1', 'rua 1', 'em frente ao mercado'); 
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('2222', '22', 'Bairro 2', 'rua 2', 'em frente ao armazem'); 
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('3333', '33', 'Bairro 3', 'rua 3', 'em frente ao zoologico'); 
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('4444', '44', 'Bairro 4', 'rua 4', 'em frente ao presidio'); 
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('5555', '55', 'Bairro 5', 'rua 5', 'em frente ao predio'); 
