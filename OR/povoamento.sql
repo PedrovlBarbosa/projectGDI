2ea19ec6e4c88ff641a5cb27d8e6093296ca7391
Romero <rrcb@cin.ufpe.br>
2023-04-04 12:13:12 -0300

    add more examples

--- OR/povoamento.sql
+++ OR/povoamento.sql
@@ -3,15 +3,65 @@ INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('2222', '
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('3333', '33', 'Bairro 3', 'rua 3', 'em frente ao zoologico'); 
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('4444', '44', 'Bairro 4', 'rua 4', 'em frente ao presidio'); 
 INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('5555', '55', 'Bairro 5', 'rua 5', 'em frente ao predio'); 
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('54321', '456', 'Bairro', 'Rua da Praia', 'Casa 01');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('23456', '789', 'Cidade', 'Rua do Sol', 'Bloco 02');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('34567', '1011', 'Valejo', 'Rua do Campo', 'Casa 05');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('45678', '1213', 'Curado', 'Rua das Flores', 'Apto 102');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('56789', '1415', 'Luzita', 'Rua do Bosque', 'Casa 07');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('67890', '1617', 'Pirica', 'Rua do Prado', 'Apto 103');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('78901', '1819', 'Louro', 'Rua da Esquina', 'Casa 10');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('89012', '2021', 'Parati', 'Rua do Mar', 'Apto 104');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('90123', '2223', 'Macaio', 'Rua do Lago', 'Casa 11');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('01234', '2425', 'Piriba', 'Rua do Sol', 'Apto 105');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('98765', '2627', 'Marita', 'Rua das Pedras', 'Casa 12');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('87654', '2829', 'Natali', 'Rua do Prado', 'Apto 106');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('76543', '3031', 'Sulina', 'Rua do Mar', 'Casa 14');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('65432', '3233', 'Ipiria', 'Rua das Flores', 'Apto 107');
+
+INSERT INTO tb_endereco(cep, numero, bairro, rua, complemento) VALUES ('54321', '3435', 'Lobato', 'Rua do Bosque', 'Casa 15');
 
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1111', '111111111', '81');
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('2222', '222222222', '81');
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('3333', '333333333', '81');
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('4444', '444444444', '81');
 INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('5555', '555555555', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('6666', '666666666', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('7777', '777777777', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('8888', '888888888', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('9999', '999999999', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1010', '101010101', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1212', '121212121', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1313', '131313131', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1414', '141414144', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1515', '151515151', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1616', '161616161', '81');
+INSERT INTO tb_telefone_pessoa(cpf_pessoa, numero, ddd) VALUES ('1717', '171717171', '81');
 
-INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1111', '211111111', '81');
-INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('2222', '322222222', '91');
-INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('3333', '433333333', '82');
-INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('4444', '544444444', '16');
-INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('5555', '655555555', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('2222', '32222222', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('3333', '43333333', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('4444', '54444444', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('5555', '65555555', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('6666', '76666666', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('7777', '87777777', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('8888', '98888888', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('9999', '99999999', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1010', '11010101', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1111', '12111111', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1212', '13212121', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1313', '14313131', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1414', '15414141', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1515', '16515151', '81');
+INSERT INTO tb_telefone_fabrica(cnpj_fabrica, numero, ddd) VALUES ('1616', '17616161', '81');
