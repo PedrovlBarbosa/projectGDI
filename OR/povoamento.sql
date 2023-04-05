1ce7207a942e17edfd92bb08c98a070a7486696b
Douglas Santos <55150864+userdouglas@users.noreply.github.com>
2023-04-04 22:11:29 -0300

    Update povoamento.sql

--- OR/povoamento.sql
+++ OR/povoamento.sql
@@ -46,7 +46,7 @@ INSERT INTO tb_fabrica VALUES ('35852147698',
                                (SELECT REF(E) FROM tb_endereco E WHERE E.cep = '333' ));
      
 INSERT INTO tb_cliente VALUES (
-		'53267498565','renan da towner',22,'renanzinhos2@gmail.com',
+		'53267498565','renan da towner',38,'renanzinhos2@gmail.com',
           tp_array_telefone_pessoa(
             tp_telefone_pessoa(
               '53267498565',
@@ -59,7 +59,7 @@ INSERT INTO tb_cliente VALUES (
     	(SELECT REF(E) FROM tb_endereco E WHERE E.cep = '8888' ), 2565);
 
 INSERT INTO tb_cliente VALUES (
-		'85624498565','rogerinho do ingá',22,'rogerinho@gmail.com',
+		'85624498565','rogerinho do ingá',42,'rogerinho@gmail.com',
           tp_array_telefone_pessoa(
             tp_telefone_pessoa(
               '85624498565',
@@ -69,7 +69,20 @@ INSERT INTO tb_cliente VALUES (
               '85624498565',
               '897532146',
               '81')),
-    	(SELECT REF(E) FROM tb_endereco E WHERE E.cep = '777' ), 2885);                               
+    	(SELECT REF(E) FROM tb_endereco E WHERE E.cep = '7777' ), 2885); 
+	
+INSERT INTO tb_cliente VALUES (
+		'65247498565','julinho da van',22,'julinho@gmail.com',
+          tp_array_telefone_pessoa(
+            tp_telefone_pessoa(
+              '65247498565',
+              '565932146',
+              '81'),
+            tp_telefone_pessoa(
+              '65247498565',
+              '894579146',
+              '81')),
+    	(SELECT REF(E) FROM tb_endereco E WHERE E.cep = '9999' ), 28442); 
 
 
 INSERT INTO tb_funcionario VALUES (
@@ -104,26 +117,51 @@ INSERT INTO tb_funcionario VALUES (
           (SELECT REF(E) FROM tb_endereco E WHERE E.cep = '7777' ),
           256, TO_DATE('09/11/2016', 'dd/mm/yyyy'), 2260, null); 
 
-INSERT INTO tb_modelo_carro VALUES ('GOL', 5);                              
-INSERT INTO tb_modelo_carro VALUES ('UNO', 18);
-INSERT INTO tb_modelo_carro VALUES ('ECOSPORT', 5);  
-INSERT INTO tb_modelo_carro VALUES ('FUSCA', 4);  
-INSERT INTO tb_modelo_carro VALUES ('KOMBI', 12);  
+                          
+INSERT INTO tb_modelo_carro VALUES ('Uno', 4);
+INSERT INTO tb_modelo_carro VALUES ('Sandero', 5);  
+INSERT INTO tb_modelo_carro VALUES ('Onix', 4);  
+
 
 INSERT INTO tb_carro VALUES ('1010', TO_DATE('2016', 'yyyy'), 'Amarelo', 
                             (SELECT REF(F) FROM tb_fabrica F WHERE F.cnpj = '099827371748'),
-                            (SELECT REF(M) FROM tb_modelo_carro M WHERE M.modelo = 'UNO'));
+                            (SELECT REF(M) FROM tb_modelo_carro M WHERE M.modelo = 'Uno'));
+	
+INSERT INTO tb_carro VALUES ('1033', TO_DATE('2022', 'yyyy'), 'Prata', 
+                            (SELECT REF(F) FROM tb_fabrica F WHERE F.cnpj = '25684581748'),
+                            (SELECT REF(M) FROM tb_modelo_carro M WHERE M.modelo = 'Sandero'));	
+
+INSERT INTO tb_carro VALUES ('2563', TO_DATE('2021', 'yyyy'), 'Azul', 
+                            (SELECT REF(F) FROM tb_fabrica F WHERE F.cnpj = '35852147698'),
+                            (SELECT REF(M) FROM tb_modelo_carro M WHERE M.modelo = 'Onix'));	
                             
 INSERT INTO tb_vender_promo VALUES ('56821479632584', 
         TO_DATE('09/12/2019', 'dd/mm/yyyy'),
-        25900,
-        (SELECT REF(M) FROM tb_carro M WHERE M.chassi = '1010'),
-        (SELECT REF(C) FROM tb_cliente C  WHERE C.cpf = '85624498565')
-        (SELECT REF(F) FROM tb_funcionario F WHERE F.cpf = '88587498565'),
+        (25900.00),
+        (SELECT REF(C) FROM tb_cliente C  WHERE C.cpf = '85624498565'),
+        (SELECT REF(F) FROM tb_funcionario F WHERE F.cpf = '12568749856'),
+    	(SELECT REF(M) FROM tb_carro M WHERE M.chassi = '1033'),
          tp_desconto(
-            '184844656',
-            20));                            
+            '184855356',
+            10));                          
                             
-                            
-                            
+INSERT INTO tb_vender_promo VALUES ('58742379632584', 
+        TO_DATE('09/12/2020', 'dd/mm/yyyy'),
+        (59900.00),
+        (SELECT REF(C) FROM tb_cliente C  WHERE C.cpf = '53267498565'),
+        (SELECT REF(F) FROM tb_funcionario F WHERE F.cpf = '12568749856'),
+    	(SELECT REF(M) FROM tb_carro M WHERE M.chassi = '1010'),
+         tp_desconto(
+            '5525356',
+            12));    
+	    
+INSERT INTO tb_vender_promo VALUES ('4785379632584', 
+        TO_DATE('09/09/2018', 'dd/mm/yyyy'),
+        (55998.00),
+        (SELECT REF(C) FROM tb_cliente C  WHERE C.cpf = '65247498565'),
+        (SELECT REF(F) FROM tb_funcionario F WHERE F.cpf = '88587498565'),
+    	(SELECT REF(M) FROM tb_carro M WHERE M.chassi = '2563'),
+         tp_desconto(
+            '4855356',
+            11));                                
                             
