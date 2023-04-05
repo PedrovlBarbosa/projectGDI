e212e9a28679d3966cfe1f85da14afb5889a4fec
PedrovlBarbosa <pvlb@cin.ufpe.br>
2023-04-05 20:41:33 -0300

    Update teste.sql

--- OR/teste.sql
+++ OR/teste.sql
@@ -24,4 +24,23 @@ BEGIN
     SELECT VALUE(F) INTO funcionario FROM TB_FUNCIONARIO F WHERE F.salario = 2000;
     funcionario.IMPRIMIR_INFORMACAO();
 END;
-/
+
+-- ORDER MEMBER FUNCTION
+DECLARE 
+    aux NUMBER;
+    idade1 tp_pessoa;
+    idade2 tp_pessoa;
+BEGIN
+    SELECT VALUE(P) INTO idade1 FROM tb_pessoa P WHERE CPF = '65247498565';
+    SELECT VALUE(P) INTO idade2 FROM tb_pessoa P WHERE CPF = '85624498565';
+    aux := idade1.COMPARA_IDADE(idade2);
+    IF aux = 1 THEN
+        DBMS_OUTPUT.PUT_LINE('A idade de: ' || TO_CHAR(idade1.nome) 
+                    || ' é maior que a idade de: ' || TO_CHAR(idade2.nome));
+    ELSIF aux = -1 THEN 
+        DBMS_OUTPUT.PUT_LINE('A idade de: ' || TO_CHAR(idade2.nome) 
+                    || ' é maior que a idade de: ' || TO_CHAR(idade1.nome));            
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('A idade de ambos são iguais.');
+    END IF;
+END;
