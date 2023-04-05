469918543945f35a01dc70aeece7b3c66c0e1665
Douglas Santos <55150864+userdouglas@users.noreply.github.com>
2023-04-05 20:36:56 -0300

    Update teste.sql

--- OR/teste.sql
+++ OR/teste.sql
@@ -1,12 +1,22 @@
--- MEMBER FUNCTION
+-- MAP MEMBER FUNCTION get_cep()
+DECLARE cp tp_endereco;
+BEGIN
+SELECT VALUE(e) INTO cp FROM tb_endereco e
+WHERE e.rua = 'rua soldadinho';
+cp.get_cep();
+END;
+/
+
+-- MEMBER FUNCTION get_salario()
 SELECT F.GET_SALARIO() FROM tb_funcionario F WHERE F.matricula = '88587498565';
 
--- MEMBER PROCEDURE
+-- MEMBER PROCEDURE 
 DECLARE pessoa TP_PESSOA;
 BEGIN
     SELECT VALUE(P) INTO pessoa FROM tb_pessoa P WHERE P.cpf = '53267498565';
     pessoa.IMPRIMIR_INFORMACAO();
 END;
+/
 
 -- OVERRIDING MEMBER PROCEDURE
 DECLARE funcionario TP_FUNCIONARIO;
@@ -14,4 +24,4 @@ BEGIN
     SELECT VALUE(F) INTO funcionario FROM TB_FUNCIONARIO F WHERE F.salario = 2000;
     funcionario.IMPRIMIR_INFORMACAO();
 END;
-
+/
