c365f1518f3133b81124e7bff768b3bf0f89feac
PedrovlBarbosa <pvlb@cin.ufpe.br>
2023-04-05 23:19:36 -0300

    updateTeste

--- OR/teste.sql
+++ OR/teste.sql
@@ -2,31 +2,23 @@
 SELECT e.get_cep() FROM tb_endereco e WHERE e.rua = 'rua soldadinho';
 
 -- MEMBER FUNCTION get_salario()
-SELECT F.GET_SALARIO() FROM tb_funcionario F WHERE F.matricula = '88587498565';
-
--- MEMBER PROCEDURE 
-DECLARE pessoa TP_PESSOA;
-BEGIN
-    SELECT VALUE(P) INTO pessoa FROM tb_pessoa P WHERE P.cpf = '53267498565';
-    pessoa.IMPRIMIR_INFORMACAO();
-END;
-/
+SELECT F.GET_SALARIO() FROM tb_funcionario F WHERE F.matricula = 256;
 
 -- OVERRIDING MEMBER PROCEDURE
 DECLARE funcionario TP_FUNCIONARIO;
 BEGIN
-    SELECT VALUE(F) INTO funcionario FROM TB_FUNCIONARIO F WHERE F.salario = 2000;
+    SELECT VALUE(F) INTO funcionario FROM TB_FUNCIONARIO F WHERE salario = 2000;
     funcionario.IMPRIMIR_INFORMACAO();
 END;
 
--- ORDER MEMBER FUNCTION
+-- OVERRIDING/ORDER MEMBER FUNCTION
 DECLARE 
     aux NUMBER;
-    idade1 tp_pessoa;
-    idade2 tp_pessoa;
+    idade1 tp_cliente;
+    idade2 tp_cliente;
 BEGIN
-    SELECT VALUE(P) INTO idade1 FROM tb_pessoa P WHERE CPF = '65247498565';
-    SELECT VALUE(P) INTO idade2 FROM tb_pessoa P WHERE CPF = '85624498565';
+    SELECT VALUE(C) INTO idade1 FROM tb_cliente C WHERE C.CPF = '65247498565';
+    SELECT VALUE(C) INTO idade2 FROM tb_cliente C WHERE C.CPF = '85624498565';
     aux := idade1.COMPARA_IDADE(idade2);
     IF aux = 1 THEN
         DBMS_OUTPUT.PUT_LINE('A idade de: ' || TO_CHAR(idade1.nome) 
