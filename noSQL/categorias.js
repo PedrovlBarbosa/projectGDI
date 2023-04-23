0c9a9b5145ecc73df260ef707dc1e714a8c4809f
Douglas Santos <55150864+userdouglas@users.noreply.github.com>
2023-04-23 19:04:04 -0300

    Update categorias.js

--- noSQL/categorias.js
+++ noSQL/categorias.js
@@ -2,51 +2,72 @@ db.createCollection("categorias");
 db.categorias.insertMany([
     {
         "name": "vestidos",
-        "fabricado": "brasil",
+        "fabricado": "Brasil",
         "marca": "Floratta Modas",
         "ano": "2022",
         "genero": "feminino"
     },
     {
         "name": "blusas",
-        "fabricado": "brasil",
+        "fabricado": "Brasil",
         "marca": "Arte em Crochê",
         "ano": "2021",
         "genero": "feminino"
     },
     {
         "name": "saias",
-        "fabricado": "argentina",
+        "fabricado": "Argentina",
         "marca": "Moda Feminina",
         "ano": "2021",
         "genero": "feminino"
     },
     {
         "name": "camisetas",
-        "fabricado": "china",
+        "fabricado": "China",
         "marca": "Basic Wear",
         "ano": "2021",
         "genero": "masculino"
     },
     {
         "name": "calcas",
-        "fabricado": "india",
+        "fabricado": "India",
         "marca": "Denim Company",
         "ano": "2020",
         "genero": "masculino"
     },
     {
         "name": "bermudas",
-        "fabricado": "bangladesh",
+        "fabricado": "Bangladesh",
         "marca": "Outdoor Adventures",
         "ano": "2022",
         "genero": "masculino"
     },
     {
         "name": "camisa",
-        "fabricado": "brasil",
+        "fabricado": "Brasil",
         "marca": "Hering",
         "ano": "2023",
         "genero": "masculino"
-    }
+    },
+    {
+        "name": "shorts",
+        "fabricado" : "China",
+        "marca" : "Dyingo",
+        "ano" : "2023",
+        "genero" : "feminino"
+    },
+    {
+        "name": "cuecas",
+        "fabricado" : "Brasil",
+        "marca" : "lacoste",
+        "ano" : "2023",
+        "genero" : "masculino"
+    },
+    {
+        "name": "lingerie",
+        "fabricado": "Brasil",
+        "marca": "zara",
+        "ano" : "2023",
+        "genero" : "feminino"
+    }  
 ]);
