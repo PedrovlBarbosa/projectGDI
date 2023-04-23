6dc905e43a1e0139929795ccd3929d7cc37c22d1
PedrovlBarbosa <pvlb@cin.ufpe.br>
2023-04-23 01:19:00 -0300

    update

--- noSQL/categorias.js
+++ noSQL/categorias.js
@@ -1,6 +1,52 @@
 db.createCollection("categorias");
 db.categorias.insertMany([
     {
-        
+        "name": "vestidos",
+        "fabricado": "brasil",
+        "marca": "Floratta Modas",
+        "ano": "2022",
+        "genero": "feminino"
+    },
+    {
+        "name": "blusas",
+        "fabricado": "brasil",
+        "marca": "Arte em Crochê",
+        "ano": "2021",
+        "genero": "feminino"
+    },
+    {
+        "name": "saias",
+        "fabricado": "argentina",
+        "marca": "Moda Feminina",
+        "ano": "2021",
+        "genero": "feminino"
+    },
+    {
+        "name": "camisetas",
+        "fabricado": "china",
+        "marca": "Basic Wear",
+        "ano": "2021",
+        "genero": "masculino"
+    },
+    {
+        "name": "calcas",
+        "fabricado": "india",
+        "marca": "Denim Company",
+        "ano": "2020",
+        "genero": "masculino"
+    },
+    {
+        "name": "bermudas",
+        "fabricado": "bangladesh",
+        "marca": "Outdoor Adventures",
+        "ano": "2022",
+        "genero": "masculino"
+    },
+    {
+        "name": "camisa",
+        "fabricado": "brasil",
+        "marca": "Hering",
+        "ano": "2023",
+        "genero": "masculino"
     }
-])
+]);
---
+++ noSQL/loja.js
@@ -0,0 +1,84 @@
+db.createCollection("loja");
+db.lojas.insertOne({
+    "nome": "Loja 1",
+    "endereco": "Rua das Flores, 123 - Centro, São Paulo - SP",
+    "produtos": [
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Vestido Floral"})._id
+            },
+            "quantidade": 50
+        },
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Camiseta Basica"})._id
+            },
+            "quantidade": 30
+        },
+    ]
+});
+
+db.lojas.insertOne({
+    "nome": "Loja 2",
+    "endereco": "Avenida Fashion, 456 - Paulista, Paulista - PE",
+    "produtos": [
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Calsa Jeans Skinny"})._id
+            },
+            "quantidade": 20
+        },
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Blusa de Croche"})._id
+            },
+            "quantidade": 40
+        },
+    ]
+});
+
+db.lojas.insertOne({
+    "nome": "Loja 3",
+    "endereco": "Avenida da Aventura, 456 - Vila Aventureira, Rio de Janeiro - RJ",
+    "produtos": [
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Bermuda Cargo"})._id
+            },
+            "quantidade": 25
+        },
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Saia Plissada"})._id
+            },
+            "quantidade": 35
+        },
+    ]
+});
+
+db.lojas.insertOne({
+    "nome": "Loja 4",
+    "endereco": "Avenida General Manoel Rabelo, 1111 - Cavaleiro, Jabotão dos Guararapes - PE",
+    "produtos": [
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Camisa Social"})._id
+            },
+            "quantidade": 15
+        },
+        {
+            "produto": {
+                $ref: "catalogo",
+                $id: db.catalogo.findOne({name: "Saia Plissada"})._id
+            },
+            "quantidade": 35
+        },
+    ]
+});
